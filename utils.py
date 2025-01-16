82331153096e7026d21bfeab2abeddca48d25268
Donatella Genovese <99698711+DonatellaGenovese@users.noreply.github.com>
2025-01-16 16:44:40 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,28 @@
+class EmbedEncode(nn.Module):
+    def __init__(self, input_size, hidden_size, encoding_size, g_norm):
+        super().__init__()
+        self.embedding = nn.Linear(input_size, hidden_size)
+        self.encoding = nn.Linear(encoding_size, hidden_size)
+        self.g_norm = g_norm
 
+    def forward(self, x):
+        if self.g_norm == True:
+          x = self.embedding(x.data_norm) + self.encoding(x.pe)
+        else:
+          x = self.embedding(x.x) + self.encoding(x.pe)
+
+        return x
+
+
+class LoadBalancingLoss(nn.Module):
+    def __init__(self, criterion, w_load):
+        super().__init__()
+        self.crit = criterion
+        self.w_load = w_load
+
+    def forward(self, output, labels):
+        predictions, loads, discard, discard_ = output
+        C = self.crit(predictions, labels)
+        LBL = self.w_load * loads
+
+        return C + LBL
